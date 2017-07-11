e3a25cfda8baaf0dd91bd7bd20302e9fb9221f38
dnicanix <Amor143!>
2017-07-11 14:07:02 +0800

    New Spring Framework Backend

---
+++ Backend/securdeonlinelibrary/src/main/java/edu/securde/SecurdeonlinelibraryApplication.java
@@ -0,0 +1,12 @@
+package edu.securde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurdeonlinelibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurdeonlinelibraryApplication.class, args);
+	}
+}
